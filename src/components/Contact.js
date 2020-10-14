4c30ba750421bfbff0bf57e87cd5fe9f44335f62
Emilie Anglade <anglade.em@gmail.com>
2020-10-14 14:26:36 +0200

    atelier jsx advanced

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function Contact(props) {
     return(
-        <div className="Contact">
+        <div className={props.star ? "star" : "Contact"}>
             <img className="avatar" src={props.avatar} alt={props.name}/>
             <div>
                 <h4 className="name">{props.name}</h4>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,38 +1,51 @@
 import React from 'react'
 import Contact from "./Contact";
 
+
 const users = [
     {
       name: 'Robert Reyes',
       avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
+      online: false,
+      star: false
     },
     {
       name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
+      //avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+      online: true,
+      star: true
     },
     {
       name: 'Vernon Mason',
       avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+      online: true,
+      star:true
     },
     {
       name: 'Erica Hunt',
       avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
+      online: false,
+      star: false
     },
     {
       name: 'Juanita Phillips',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+      //avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+      online: true,
+      star: false
     }
   ];
+
+  const placeholder = "https://www.drupal.org/files/issues/default-avatar.png"
 
   const ContactList = () => (
     <div>
       {users.map(element => (
-        <Contact key={element.name} name={element.name} avatar={element.avatar} online={element.online} />
+        <Contact 
+        key={element.name} 
+        name={element.name} 
+        avatar={element.avatar ? element.avatar : placeholder} 
+        online={element.online}
+        star={element.star} />
       ))}
     </div>
   );

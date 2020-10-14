9ea74946a1c9e8383365160fdc50569717490829
Emilie Anglade <anglade.em@gmail.com>
2020-10-14 10:49:33 +0200

    challenge contactlist avec map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-      avatar="https://randomuser.me/api/portraits/women/82.jpg" 
-      name="Gertrude Wood"
-      online={true}
-      />
-      <Contact 
-      avatar="https://randomuser.me/api/portraits/men/59.jpg" 
-      name="Daryl Fox"
-      online={false}
-      />
-      <Contact 
-      avatar="https://randomuser.me/api/portraits/women/7.jpg" 
-      name="Jamie Stephens"
-      online={true}
-      />
+      <ContactList />
     </div>
   );
 }
